4583a96c5df1bd3271285fdf0b72daba305bf567
Achmad Fatkharrofiqi <achmadfatkkharrofiqi404@gmail.com>
2022-08-31 14:44:07 +0000

    [ADD] trending card

--- src/components/Content/Trending.tsx
+++ src/components/Content/Trending.tsx
@@ -1,9 +1,25 @@
-import { Box, Flex, HStack, Text } from '@chakra-ui/react'
+import {
+  AspectRatio,
+  Box,
+  Divider,
+  Flex,
+  Grid,
+  GridItem,
+  HStack,
+  Image,
+  Stack,
+  Text,
+} from '@chakra-ui/react'
 import { FC } from 'react'
-import { BsArrowRightShort } from 'react-icons/bs'
 import { trending } from '../../data/trending'
 import SimpleBar from 'simplebar-react'
 import 'simplebar/dist/simplebar.min.css'
+import { FiArrowRight } from 'react-icons/fi'
+import { HiBadgeCheck } from 'react-icons/hi'
+import { BsHeart, BsHeartFill } from 'react-icons/bs'
+import { GoKebabVertical } from 'react-icons/go'
+import { FaRegComment } from 'react-icons/fa'
+import moment from 'moment'
 
 const Trending: FC = () => {
   return (
@@ -11,18 +27,19 @@ const Trending: FC = () => {
       <Box
         borderTopRadius={10}
         bgColor="white"
-        p={2}
+        py={2}
         borderBottomWidth={2}
         borderBottomColor="gray.100"
       >
         <Flex
           justifyContent="space-between"
           position="relative"
+          px={3}
           _before={{
             position: 'absolute',
-            left: '-10px',
-            right: '5px',
-            top: '0.3rem',
+            left: 0,
+            top: 1.5,
+            bottom: 0,
             width: '0.4rem',
             borderRadius: '2px',
             height: '24px',
@@ -34,21 +51,63 @@ const Trending: FC = () => {
             Trending
           </Text>
           <HStack>
-            <Text color="gray">Lainnya</Text>
-            <BsArrowRightShort />
+            <Text color="blue.400">Lihat Lainnya</Text>
+            <FiArrowRight color="gray" />
           </HStack>
         </Flex>
       </Box>
       <SimpleBar
         style={{
-          maxHeight: '55vh',
+          maxHeight: '70vh',
         }}
       >
         <Box borderBottomRadius={10} bgColor="white" p={2}>
           {trending.map((data, index) => {
             return (
-              <Box key={index} h={100}>
-                <Text>{data.title}</Text>
+              <Box key={index} m={2}>
+                <Grid templateColumns="repeat(4, 1fr)" gap={2}>
+                  <GridItem colSpan={3}>
+                    <Stack direction="column">
+                      <Text fontWeight="500">{data.title}</Text>
+                      <Stack direction="row" alignItems="center">
+                        <Image
+                          src={data.avatar}
+                          borderRadius="full"
+                          border={'1px solid gray'}
+                          boxSize="1rem"
+                        />
+                        <Text>{data.createdBy}</Text>
+                        <HiBadgeCheck color="teal" />
+                      </Stack>
+                      <Flex direction="row" alignItems="center" gap={7}>
+                        <Stack direction="row" alignItems="center">
+                          {data.totalLikes > 0 ? (
+                            <BsHeartFill color="red" />
+                          ) : (
+                            <BsHeart />
+                          )}
+                          <Text fontSize="sm">{data.totalLikes}</Text>
+                        </Stack>
+                        <Stack direction="row" alignItems="center">
+                          <FaRegComment />
+                          <Text fontSize="sm">{data.totalComments}</Text>
+                        </Stack>
+                        <Stack direction="row" alignItems="center">
+                          <Text fontSize="sm">
+                            {moment(data.lastUpdated).format('DD MMM')}
+                          </Text>
+                        </Stack>
+                        <GoKebabVertical />
+                      </Flex>
+                    </Stack>
+                  </GridItem>
+                  <GridItem>
+                    <AspectRatio ratio={4 / 4}>
+                      <Image src={data.image} rounded={10} />
+                    </AspectRatio>
+                  </GridItem>
+                </Grid>
+                <Divider my={3} />
               </Box>
             )
           })}
--- src/models/Trending.ts
+++ src/models/Trending.ts
@@ -1,10 +1,11 @@
 export interface Trending {
-  title: string,
-  createdBy: string,
-  like: boolean,
-  totalLikes: number,
-  comment: boolean,
-  totalComments: number,
-  lastUpdated: Date,
+  title: string
+  createdBy: string
+  avatar: string
+  like: boolean
+  totalLikes: number
+  comment: boolean
+  totalComments: number
+  lastUpdated: Date
   image: string
 }
