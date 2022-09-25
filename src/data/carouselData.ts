0732d11bcf2d2131df2fb123676818387907afe6
Achmad Fatkharrofiqi <achmadfatkharrofiqi404@gmail.com>
2022-09-25 05:53:48 +0000

    add: more data carousel

--- src/data/carouselData.ts
+++ src/data/carouselData.ts
@@ -1,9 +1,10 @@
-import CarouselItem from "@models/CarouselItem"
+import CarouselItem from '@models/CarouselItem'
 
 export const carouselData: CarouselItem[] = [
   {
     bigContent: {
-      title: 'Pasukan Israel Tembak Mati Sopir Palestina, Dituduh Berusaha Tabrak Mobil Polisi',
+      title:
+        'Pasukan Israel Tembak Mati Sopir Palestina, Dituduh Berusaha Tabrak Mobil Polisi',
       createdBy: 'kumparanNEWS',
       avatar:
         'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_32,ar_1:1/v1512588407/tufp3tpktdtkkk9uzl25.png',
@@ -17,7 +18,8 @@ export const carouselData: CarouselItem[] = [
     },
     smallContent: [
       {
-        title: 'Panglima TNI Merasa Salah Tak Ada Putra Dayak Jadi Perwira Tingg, Siap Upayakan',
+        title:
+          'Panglima TNI Merasa Salah Tak Ada Putra Dayak Jadi Perwira Tingg, Siap Upayakan',
         createdBy: 'kumparanNEWS',
         avatar:
           'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_32,ar_1:1/v1512588407/tufp3tpktdtkkk9uzl25.png',
@@ -27,10 +29,11 @@ export const carouselData: CarouselItem[] = [
         totalLikes: 1,
         lastUpdated: new Date(),
         image:
-          'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1644313028/xgl2l73pz7uk8enklukf.jpg'
+          'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1644313028/xgl2l73pz7uk8enklukf.jpg',
       },
       {
-        title: 'Mahfud MD Soroti Wakil Ketua DPRD Depok Suruh Sopir Truk Push Up di Jalan',
+        title:
+          'Mahfud MD Soroti Wakil Ketua DPRD Depok Suruh Sopir Truk Push Up di Jalan',
         createdBy: 'kumparanNEWS',
         avatar:
           'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_32,ar_1:1/v1512588407/tufp3tpktdtkkk9uzl25.png',
@@ -42,6 +45,98 @@ export const carouselData: CarouselItem[] = [
         image:
           'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1634025439/018123317250bade60d86e781b9f187e.jpg',
       },
-    ]
-  }
+    ],
+  },
+  {
+    bigContent: {
+      title:
+        'Pasukan Israel Tembak Mati Sopir Palestina, Dituduh Berusaha Tabrak Mobil Polisi',
+      createdBy: 'kumparanNEWS',
+      avatar:
+        'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_32,ar_1:1/v1512588407/tufp3tpktdtkkk9uzl25.png',
+      comment: true,
+      totalComments: 4,
+      like: true,
+      totalLikes: 1,
+      lastUpdated: new Date(),
+      image:
+        'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_480,ar_16:9/v1615764266/umyecka1ohrxv4vwqnnj.jpg',
+    },
+    smallContent: [
+      {
+        title:
+          'Panglima TNI Merasa Salah Tak Ada Putra Dayak Jadi Perwira Tingg, Siap Upayakan',
+        createdBy: 'kumparanNEWS',
+        avatar:
+          'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_32,ar_1:1/v1512588407/tufp3tpktdtkkk9uzl25.png',
+        comment: true,
+        totalComments: 4,
+        like: true,
+        totalLikes: 1,
+        lastUpdated: new Date(),
+        image:
+          'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1644313028/xgl2l73pz7uk8enklukf.jpg',
+      },
+      {
+        title:
+          'Mahfud MD Soroti Wakil Ketua DPRD Depok Suruh Sopir Truk Push Up di Jalan',
+        createdBy: 'kumparanNEWS',
+        avatar:
+          'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_32,ar_1:1/v1512588407/tufp3tpktdtkkk9uzl25.png',
+        comment: true,
+        totalComments: 4,
+        like: true,
+        totalLikes: 1,
+        lastUpdated: new Date(),
+        image:
+          'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1634025439/018123317250bade60d86e781b9f187e.jpg',
+      },
+    ],
+  },
+  {
+    bigContent: {
+      title:
+        'Pasukan Israel Tembak Mati Sopir Palestina, Dituduh Berusaha Tabrak Mobil Polisi',
+      createdBy: 'kumparanNEWS',
+      avatar:
+        'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_32,ar_1:1/v1512588407/tufp3tpktdtkkk9uzl25.png',
+      comment: true,
+      totalComments: 4,
+      like: true,
+      totalLikes: 1,
+      lastUpdated: new Date(),
+      image:
+        'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_480,ar_16:9/v1615764266/umyecka1ohrxv4vwqnnj.jpg',
+    },
+    smallContent: [
+      {
+        title:
+          'Panglima TNI Merasa Salah Tak Ada Putra Dayak Jadi Perwira Tingg, Siap Upayakan',
+        createdBy: 'kumparanNEWS',
+        avatar:
+          'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_32,ar_1:1/v1512588407/tufp3tpktdtkkk9uzl25.png',
+        comment: true,
+        totalComments: 4,
+        like: true,
+        totalLikes: 1,
+        lastUpdated: new Date(),
+        image:
+          'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1644313028/xgl2l73pz7uk8enklukf.jpg',
+      },
+      {
+        title:
+          'Mahfud MD Soroti Wakil Ketua DPRD Depok Suruh Sopir Truk Push Up di Jalan',
+        createdBy: 'kumparanNEWS',
+        avatar:
+          'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_32,ar_1:1/v1512588407/tufp3tpktdtkkk9uzl25.png',
+        comment: true,
+        totalComments: 4,
+        like: true,
+        totalLikes: 1,
+        lastUpdated: new Date(),
+        image:
+          'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1634025439/018123317250bade60d86e781b9f187e.jpg',
+      },
+    ],
+  },
 ]
